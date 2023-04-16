08511c80ead185fe2e0136f8521280e7a9de1ab8
guilh <guilherme00rogerio@gmail.com>
2023-04-16 14:07:06 -0300

    alterações semânticas e otimização do código

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,31 +3,32 @@ import { useState } from 'react';
 import './styles.css'
 
 function Header() {
-    const[isScrolled, setIsScrolled] = useState(false);
 
-    window.onscroll = () => {
-      setIsScrolled(window.pageYOffset === 0 ? false : true)
-      return () => (window.onscroll = null)
-    }
+  const HeaderList = [
+    { id: 1, urlName: 'Home', url:'#home' },
+    { id: 2, urlName: 'Sobre mim', url: '#sobre'},
+    { id: 3, urlName: 'Projetos', url: '#projetos'},
+    { id: 4, urlName: 'Contato', url: '#contato'}
+  ]
+
+  const HeaderLinks = HeaderList.map((props) => 
+    <div className="header-infos" key={props.id}>
+      <a href={props.url}>{props.urlName}</a>
+    </div>
+  )
+
+  const[isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    return () => (window.onscroll = null)
+  }
 
   return (
     <header className={isScrolled ? 'scrolled' : 'header-container'}>
         <a href="#home"><h1>GN</h1></a>
         <div className='header-infos'>
-          <a href='#sobre'>Sobre mim</a>
-          <a href='#projetos'>Projetos</a>
-          <a href='#contato'>contato</a>
-        </div>
-
-        <div className="header-hamburguer">
-          <div className="hamburguer-1"> </div>
-          <div className="hamburguer-2"> </div>
-          <div className="hamburguer-3"> </div>
-        </div>
-        <div className='header-hamburguer-links'>
-          <a href='#sobre'>Sobre mim</a>
-          <a href='#projetos'>Projetos</a>
-          <a href='#contato'>contato</a>
+          {HeaderLinks}
         </div>
     </header>
   )
