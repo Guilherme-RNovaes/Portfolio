7008657d20f1ff330b707c08a023bdc62ea0e7b0
guilh <guilherme00rogerio@gmail.com>
2023-04-17 19:55:40 -0300

    Pequenas alterações feitas nos media queries

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -15,33 +15,41 @@ function About() {
         </section>
         <h1 className='skills-title'>Minhas Skills</h1>
         <section className='skills-container-items'>
-            <div className="container-item">
-                <DiHtml5 size={100} />
-                <h2>HTML</h2>
+            <div className="container-items">
+                <div className="container-item">
+                    <DiHtml5 size={100} />
+                    <h2>HTML</h2>
+                </div>
+                <div className="container-item">
+                    <DiCss3 size={100} />
+                    <h2>CSS</h2>
+                </div>
             </div>
-            <div className="container-item">
-                <DiCss3 size={100} />
-                <h2>CSS</h2>
+            <div className="container-items">
+                <div className="container-item">
+                    <DiJavascript size={100} />
+                    <h2>JavaScript</h2>
+                </div>
+                <div className="container-item">
+                    <DiReact size={100} />
+                    <h2>React</h2>
+                </div>
             </div>
-            <div className="container-item">
-                <DiJavascript size={100} />
-                <h2>JavaScript</h2>
+            <div className="container-items">
+                <div className="container-item">
+                    <DiNodejs size={100}/>
+                    <h2>Nodejs</h2>
+                </div>
             </div>
-            <div className="container-item">
-                <DiReact size={100} />
-                <h2>React</h2>
-            </div>
-            <div className="container-item">
-                <DiNodejs size={100}/>
-                <h2>Nodejs</h2>
-            </div>
-            <div className="container-item">
-                <DiBootstrap size={100}/>
-                <h2>Bootstrap</h2>
-            </div>
-            <div className="container-item">
-                <SiTailwindcss size={100}/>
-                <h2>Tailwind</h2>
+            <div className="container-items">
+                <div className="container-item">
+                    <DiBootstrap size={100}/>
+                    <h2>Bootstrap</h2>
+                </div>
+                <div className="container-item">
+                    <SiTailwindcss size={100}/>
+                    <h2>Tailwind</h2>
+                </div>
             </div>
         </section>
     </main>
