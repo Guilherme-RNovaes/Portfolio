939868f5f52873d87ac77aedd8a7b93fddb4bbfd
guilh <guilherme00rogerio@gmail.com>
2023-04-27 19:59:29 -0300

    Finalizando o componente Skills e edições em alguns arquivos css

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,7 +1,4 @@
 import './styles.css'
-import { DiBootstrap, DiCss3, DiHtml5, DiJavascript, DiNodejs, DiReact } from "react-icons/di";
-import { SiTailwindcss } from "react-icons/si"
-
 
 function About() {
   return (
@@ -12,45 +9,6 @@ function About() {
                 <p>Olá, eu me chamo Guilherme Rogério Novaes, tenho 22 anos e estudo desenvolvimento front-end a 2 anos, desenvolvendo projetos particulares e estudando de forma autodidata.</p>
                 <p>No momento estou buscando oportunidades de emprego na área, aceito qualquer tipo de desafio e oportunidades.</p> 
                 <p>Atualmente sou desenvolvedor front-end e desenvolvo em ReactJs. Estou a cada dia me aperfeiçoando mais, mantendo o foco e consistência nos estudos e prática.</p>
-            </div>
-        </section>
-        <h1 className='skills-title'>Minhas Skills</h1>
-        <section className='skills-container-items'>
-            <div className="container-items">
-                <div className="container-item">
-                    <DiHtml5 size={100} />
-                    <h2>HTML</h2>
-                </div>
-                <div className="container-item">
-                    <DiCss3 size={100} />
-                    <h2>CSS</h2>
-                </div>
-            </div>
-            <div className="container-items">
-                <div className="container-item">
-                    <DiJavascript size={100} />
-                    <h2>JavaScript</h2>
-                </div>
-                <div className="container-item">
-                    <DiReact size={100} />
-                    <h2>React</h2>
-                </div>
-            </div>
-            <div className="container-items">
-                <div className="container-item">
-                    <DiNodejs size={100}/>
-                    <h2>Nodejs</h2>
-                </div>
-            </div>
-            <div className="container-items">
-                <div className="container-item">
-                    <DiBootstrap size={100}/>
-                    <h2>Bootstrap</h2>
-                </div>
-                <div className="container-item">
-                    <SiTailwindcss size={100}/>
-                    <h2>Tailwind</h2>
-                </div>
             </div>
         </section>
     </main>
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,9 +1,68 @@
 import { useState } from 'react';
+import { DiBootstrap, DiCss3, DiHtml5, DiJavascript, DiMysql, DiReact } from "react-icons/di";
+import { SiTailwindcss, SiNodedotjs, SiMongodb, SiDocker } from "react-icons/si"
+import { BsCheck2Circle } from "react-icons/bs"
 
 import './styles.css'
 
 function Skills() {
+
+  const FrontList = [
+    { id: 1, ico: <DiHtml5 size={30} />, skillName: 'Html', skillLevel: 'Avançado' },
+    { id: 2, ico: <DiCss3 size={30} />, skillName: 'Css', skillLevel: 'Avançado' },
+    { id: 3, ico: <DiBootstrap size={30} />, skillName: 'Bootstrap', skillLevel: 'Iniciante' },
+    { id: 4, ico: <SiTailwindcss size={30} />, skillName: 'Tailwind', skillLevel: 'Iniciante' },
+    { id: 5, ico: <DiJavascript size={30} />, skillName: 'Javascript', skillLevel: 'Intermediário' },
+    { id: 6, ico: <DiReact size={30} />, skillName: 'React', skillLevel: 'Intermediário' }
+
+  ]
+
+  const BackList = [
+    { id: 1, ico: <SiNodedotjs size={30} />, skillName: 'NodeJs', skillLevel: 'Iniciante' },
+    { id: 2, ico: <SiMongodb size={30} />, skillName: 'MongoDb', skillLevel: 'Iniciante' },
+    { id: 3, ico: <DiMysql size={30} />, skillName: 'MySql', skillLevel: 'Iniciante' },
+    { id: 4, ico: <SiDocker size={30} />, skillName: 'Docker', skillLevel: 'Iniciante' }
+  ]
+
+  const PersonalList = [
+    { id: 1, ico: <BsCheck2Circle size={30} />, skillName: 'Proativo' },
+    { id: 2, ico: <BsCheck2Circle size={30} />, skillName: 'Flexível' },
+    { id: 3, ico: <BsCheck2Circle size={30} />, skillName: 'Criativo' },
+    { id: 4, ico: <BsCheck2Circle size={30} />, skillName: 'Dinâmico' },
+    { id: 5, ico: <BsCheck2Circle size={30} />, skillName: 'Resiliente' },
+    { id: 6, ico: <BsCheck2Circle size={30} />, skillName: 'Organizado' }
+  ]
   
+  const FrontSkillsList = FrontList.map((props) =>
+    <div className='skill-list'>
+        <i>{props.ico}</i>
+        <div className="skill-title">
+            <h3>{props.skillName}</h3>
+            <p>{props.skillLevel}</p>
+        </div>
+    </div>
+  ) 
+  
+  const BackSkillsList = BackList.map((props) =>
+    <div className='skill-list'>
+        <i>{props.ico}</i>
+        <div className="skill-title">
+            <h3>{props.skillName}</h3>
+            <p>{props.skillLevel}</p>
+        </div>
+    </div>
+  ) 
+  
+  const PersonalSkillsList = PersonalList.map((props) =>
+    <div className='skill-list'>
+        <i>{props.ico}</i>
+        <div className="skill-title">
+            <h3>{props.skillName}</h3>
+            <p>{props.skillLevel}</p>
+        </div>
+    </div>
+  ) 
+
   const [toogleState, setToogleState] = useState(1);
   
   const toogleTab = (index) => {
@@ -53,10 +112,7 @@ function Skills() {
                     : 'skills-content'
                 }
             >
-                <h1>HTML</h1>
-                <h1>CSS</h1>
-                <h1>JavaScript</h1>
-                <h1>ReactJs</h1>
+                {FrontSkillsList}
             </div>
             <div 
                 className={
@@ -65,9 +121,7 @@ function Skills() {
                     : 'skills-content'
                 }
             >
-                <h1>NodeJs</h1>
-                <h1>MongoDB</h1>
-                <h1>MySql</h1>
+                {BackSkillsList}
             </div>
             <div 
                 className={
@@ -76,10 +130,7 @@ function Skills() {
                     : 'skills-content'
                 }
             >
-                <h1>HTML</h1>
-                <h1>CSS</h1>
-                <h1>JavaScript</h1>
-                <h1>ReactJs</h1>
+                {PersonalSkillsList}
             </div>
         </section>
     </section>
