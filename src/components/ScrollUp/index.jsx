52f710967a4ef134347bb258b2754a4b8cfdfba8
guilh <guilherme00rogerio@gmail.com>
2023-04-29 12:33:33 -0300

    Finalizado o componente ScrollUp

--- src/components/ScrollUp/index.jsx
+++ src/components/ScrollUp/index.jsx
@@ -1,8 +1,24 @@
+import { MdKeyboardArrowUp } from "react-icons/md";
 import './styles.css'
 
 function ScrollUp() {
+
+  window.addEventListener('scroll', function() {
+    const scrollUp = document.querySelector('.scroll-up');
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        scrollUp.classList.add('max-scroll');
+    }
+    else scrollUp.classList.remove('max-scroll')
+  })
+
   return (
-    
+    <a href="#home" className="scroll-up">
+        <i>
+            <MdKeyboardArrowUp size={35}/>
+        </i>
+    </a>
   )
 }
 
