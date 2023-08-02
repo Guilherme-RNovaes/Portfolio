7594fa277e9ec3cbcfc581750b49182ef103088e
guilh <guilherme00rogerio@gmail.com>
2023-08-02 15:17:13 -0300

    Ajustes de alguns estilos e implementação da função handleScroll

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { RiCodeSSlashFill, RiContactsBookFill, RiHomeFill, RiToolsFill, RiTrophyFill, RiUser3Fill } from 'react-icons/ri'
 import './styles.css'
 
 
 function Navbar() {
-  const HeaderList = [
-    { id: 1, urlName: 'Home', ico: <RiHomeFill />, url:'#home' },
-    { id: 2, urlName: 'Sobre mim', ico: <RiUser3Fill />, url: '#sobre'},
-    { id: 3, urlName: 'Skills', ico: <RiCodeSSlashFill />, url: '#skills'},
-    { id: 4, urlName: 'Qualificação', ico: <RiTrophyFill />, url: '#qualificacao'},
-    { id: 5, urlName: 'Projetos', ico: <RiToolsFill />, url: '#projetos'},
-    { id: 6, urlName: 'Contato', ico: <RiContactsBookFill />, url: '#contato'}
-  ]
-  
+  const HeaderList = useMemo(() => [
+    { id: 1, urlName: 'Home', ico: <RiHomeFill />, url: '#home' },
+    { id: 2, urlName: 'Sobre mim', ico: <RiUser3Fill />, url: '#sobre' },
+    { id: 3, urlName: 'Skills', ico: <RiCodeSSlashFill />, url: '#skills' },
+    { id: 4, urlName: 'Qualificação', ico: <RiTrophyFill />, url: '#qualificacao' },
+    { id: 5, urlName: 'Projetos', ico: <RiToolsFill />, url: '#projetos' },
+    { id: 6, urlName: 'Contato', ico: <RiContactsBookFill />, url: '#contato' }
+  ], [])
+
   const [activeLink, setActiveLink] = useState('#home');
+
+  useEffect(() => {
+    // Função para verificar a posição do scroll e atualizar o link ativo na navbar
+    const handleScroll = () => {
+      const sections = HeaderList.map(item => document.getElementById(item.url.slice(1))); // Obtém as seções usando o URL das props
+      const scrollPosition = window.scrollY + 100; // Adicione um offset para melhorar a precisão da mudança
+
+      sections.forEach((section, index) => {
+        if (section) {
+          const sectionTop = section.offsetTop;
+          const sectionHeight = section.offsetHeight;
+
+          if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            setActiveLink(HeaderList[index].url); // Atualiza o link ativo com o URL da seção correspondente
+          }
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Remova o event listener quando o componente for desmontado
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [HeaderList]);
   
     const HeaderLinks = HeaderList.map((props) => 
       <div key={props.id}>
