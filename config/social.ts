e0cf6acb0934312239ae7270fc0a85a96f72d52e
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-05 13:28:41 -0300

    feat: add user social types

---
+++ config/social.ts
@@ -0,0 +1,29 @@
+import userProps from "./user";
+
+export type social = {
+  name: string;
+  link: string;
+}
+
+export const socialProps: social[] = [
+  {
+    name: 'Github',
+    link: 'https://github.com/Guilherme-RNovaes'
+  },
+  {
+    name: 'Linkedin',
+    link: 'https://www.linkedin.com/in/guilherme-r-novaes/'
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/gr.novaes/'
+  },
+  {
+    name: 'Email',
+    link: 'mailto:guilherme00rogerio@gmail.com'
+  },
+  {
+    name: 'Currúculo',
+    link: userProps.curriculum,
+  },
+]
