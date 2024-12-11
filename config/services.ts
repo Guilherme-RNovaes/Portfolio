278576e152dfb4228869b5be5a22588f15f2c422
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-11 09:44:51 -0300

    feat: add services array to config

---
+++ config/services.ts
@@ -0,0 +1,44 @@
+
+export interface service {
+  name: string;
+  description: string;
+  side: string;
+}
+
+export const services: service[] = [
+  {
+    name: "Criação de landing page",
+    description: "",
+    side: "left"
+  },
+  {
+    name: "Criação de site institucional",
+    description: "",
+    side: "right"
+  },
+  {
+    name: "Criação de página de captura",
+    description: "",
+    side: "left"
+  },
+  {
+    name: "Criação de página de vendas",
+    description: "",
+    side: "right"
+  },
+  {
+    name: "Criação de E-commerce",
+    description: "",
+    side: "left"
+  },
+  {
+    name: "Otimização de SEO",
+    description: "",
+    side: "right"
+  },
+  {
+    name: "Conexão/Consumo de REST API's",
+    description: "",
+    side: "left"
+  },
+]
