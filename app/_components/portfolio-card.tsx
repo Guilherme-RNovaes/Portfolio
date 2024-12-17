64ad5f9f580e683c3eacd308988f8ebf80f9e2e7
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-17 17:54:03 -0300

    feat: add changes to portfolio-card component

--- app/_components/portfolio-card.tsx
+++ app/_components/portfolio-card.tsx
@@ -17,10 +17,11 @@ type Props = {
   description: string;
   link: string;
   source: string;
-  stacks: string[];
+  stacks?: string[];
+  services: string[];
 }
 
-const PortfolioCard = ({ direction, name, img, description, stacks, link, source }: Props) => {
+const PortfolioCard = ({ direction, name, img, description, stacks, services, link, source }: Props) => {
   const isRightDirection = direction === 'right'
   return (
     <motion.div
@@ -37,27 +38,35 @@ const PortfolioCard = ({ direction, name, img, description, stacks, link, source
           <Image src={img} width={1920} height={1080} alt="project image" className="rounded-lg" />
         </DialogContent>
       </Dialog>
-      <div className={`flex flex-col items-center ${isRightDirection ? 'md:items-start' : 'md:items-end md:text-right'}`}>
-        <h1 className="pb-4 text-7xl uppercase">{name}</h1>
-        <p className="text-2xl text-zinc-600 font-light pb-4 max-w-xs md:max-w-xl truncate text-wrap">{description}</p>
-        <div className={`flex items-center justify-center ${isRightDirection ? 'md:justify-start' : 'md:justify-end'} gap-2 mb-4 flex-wrap`}>
-          {stacks.map((stack, index) => (
-            <Badge variant="outline" key={index}>
-              <p className="text-zinc-500 font-semibold hover:text-zinc-300 transition">{stack}</p>
-            </Badge>
-          ))}
+      <div className={`flex flex-col items-center gap-6 ${isRightDirection ? 'md:items-start' : 'md:items-end md:text-right'}`}>
+        <h1 className="text-7xl uppercase">{name}</h1>
+        <p className="text-2xl text-zinc-400 font-light max-w-xs md:max-w-xl truncate text-wrap">{description}</p>
+        <div className={`flex items-center justify-center ${isRightDirection ? 'md:justify-start' : 'md:justify-end'} gap-2 flex-wrap`}>
+          {stacks ? (
+            stacks.map((stacks, index) => (
+              <Badge variant="default" key={index}>
+                <p className="font-light text-lg">{stacks}</p>
+              </Badge>
+            ))
+          ) : (
+            services.map((services, index) => (
+              <Badge variant="default" key={index}>
+                <p className="font-light text-lg">{services}</p>
+              </Badge>
+            ))
+          )}
         </div>
         <div className="flex flex-col md:flex-row gap-4">
           <Button asChild>
             <Link href={link} target="_blank" rel="noreferrer noopener" className="gap-2">
               <Link2 size={18} />
-              Visit Page
+              Visitar Página
             </Link>
           </Button>
           <Button asChild variant='outline'>
             <Link href={source} target="_blank" rel="noreferrer noopener" className="gap-2">
               <Github size={18} />
-              Source code
+              Ver o código
             </Link>
           </Button>
         </div>
