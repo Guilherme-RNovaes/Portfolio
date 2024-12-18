b6c93d9669de4c8fe72ef3bc0207bdc46aa48d89
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-18 20:05:18 -0300

    feat: add skill-card component to skills section

--- app/(home)/_components/skills.tsx
+++ app/(home)/_components/skills.tsx
@@ -4,6 +4,8 @@ import { motion } from "motion/react";
 import { fromBottomAnimation } from "@/config/animations";
 import Marquee from "./marquee";
 import userProps from "@/config/user";
+import SkillCard from "./skill-card";
+import { ArrowRightCircle, Code, FileText, LifeBuoy, MonitorSmartphone, Palette } from "lucide-react";
 
 const Skills = () => {
   return (
@@ -17,6 +19,29 @@ const Skills = () => {
         <h1 className="text-5xl font-semibold">Minhas Skills</h1>
         <p className="max-w-3xl text-center font-light text-gray-500 text-2xl mb-4">{userProps.aboutSkills}</p>
       </motion.header>
+      <div className="flex flex-row items-center justify-between gap-4 mb-20">
+        <motion.div
+          variants={fromBottomAnimation({ duration: 0.7, delay: 0.5 })}
+          initial="hidden"
+          whileInView="visible"
+        >
+          <SkillCard icon={ArrowRightCircle} title="Landing Pages que convertem" text="Precisa atrair clientes? Crio páginas de alta performance, otimizadas para SEO e com design voltado para conversões." />
+        </motion.div>
+        <motion.div
+          variants={fromBottomAnimation({ duration: 0.7, delay: 0.7 })}
+          initial="hidden"
+          whileInView="visible"
+        >
+          <SkillCard icon={MonitorSmartphone} title="Design responsivo" text="Sites incríveis, que funcionam perfeitamente em qualquer dispositivo. Proporciono uma experiência fluida tanto para desktop quanto para mobile." />
+        </motion.div>
+        <motion.div
+          variants={fromBottomAnimation({ duration: 0.7, delay: 0.9 })}
+          initial="hidden"
+          whileInView="visible"
+        >
+          <SkillCard icon={LifeBuoy} title="Suporte técnico e manutenção" text="Não basta ter um site incrível, ele precisa estar sempre atualizado. Ofereço suporte contínuo para garantir que sua plataforma esteja sempre no topo." />
+        </motion.div>
+      </div>
       <motion.div
         variants={fromBottomAnimation({ duration: 0.7, delay: 0.5 })}
         initial="hidden"
