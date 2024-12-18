e0b4111bd744e5230557421c326254eea760fdfd
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-18 20:04:49 -0300

    style: add styles to skill-card component

--- app/(home)/_components/skill-card.tsx
+++ app/(home)/_components/skill-card.tsx
@@ -7,10 +7,10 @@ interface skillCardProps {
 }
 const SkillCard = ({ icon: Icon, title, text }: skillCardProps) => {
   return (
-    <div>
-      <Icon />
-      <h1>{title}</h1>
-      <p>{text}</p>
+    <div className="flex flex-col max-w-[350px] h-[450px] justify-start px-8 py-16 gap-6 border border-zinc-800 rounded-xl">
+      <Icon className="w-16 h-16" />
+      <h1 className="text-4xl font-light">{title}</h1>
+      <p className="text-xl font-light text-zinc-500">{text}</p>
     </div>
   )
 }
