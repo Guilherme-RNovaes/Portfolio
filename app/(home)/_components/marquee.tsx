5757c6fc9e62079c13cbe700a2642a7aa5cc5fda
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-27 09:47:29 -0300

    style: implement responsiveness to about section

--- app/(home)/_components/marquee.tsx
+++ app/(home)/_components/marquee.tsx
@@ -14,9 +14,9 @@ const Marquee = () => {
     <>
       {skills.map((props, index) => (
         <div key={index} className="m-2">
-          <Badge variant='secondary' className="flex flex-row items-center justify-start w-36 md:w-40 h-14 gap-2 px-4 py-2 rounded-xl">
+          <Badge variant='secondary' className="flex flex-row items-center justify-start w-32 md:w-40 h-12 md:h-14 gap-2 px-2 md:px-4 py-2 rounded-xl">
             <props.icon size={22} />
-            <p className="text-sm font-semibold">{props.skill}</p>
+            <p className="text-sm font-medium md:font-semibold">{props.skill}</p>
           </Badge>
         </div>
       ))}
@@ -24,7 +24,7 @@ const Marquee = () => {
   )
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto overflow-hidden">
       <MarqueeItem items={renderSkills(frontEnd)} from={0} to={"-100%"} />
       <MarqueeItem items={renderSkills(backEnd)} from={"-100%"} to={0} />
       <MarqueeItem items={renderSkills(dataBase)} from={0} to={"-100%"} />
