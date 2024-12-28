a47ea465f4e91e0ba078b17c142caa1a305e761c
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-28 18:39:07 -0300

    style: implement responsiveness to contact section

--- app/(home)/_components/contact.tsx
+++ app/(home)/_components/contact.tsx
@@ -6,17 +6,17 @@ import userProps from "@/config/user";
 
 const Contact = () => {
   return (
-    <div className="min-h-screen w-screen flex flex-col items-center light bg-background text-foreground pt-40 px-36">
+    <div className="min-h-screen w-screen flex flex-col items-center light bg-background text-foreground pt-20 md:pt-40 px-8 md:px-24 xl:px-36">
       <motion.header
         variants={fromBottomAnimation({ duration: 0.7, delay: 0 })}
         initial="hidden"
         whileInView="visible"
-        className="flex items-center justify-between w-full mb-40"
+        className="flex flex-col md:flex-row items-start md:items-center justify-between gap-10 md:gap-4 xl:gap-0 w-full mb-20 md:mb-40"
       >
-        <h1 className="text-9xl font-light tracking-wide">Contato</h1>
-        <div className="flex flex-col">
-          <p className="max-w-2xl text-right font-light text-3xl">Whatsapp: {userProps.cel}</p>
-          <p className="max-w-2xl text-right font-light text-3xl">{userProps.email}</p>
+        <h1 className="text-6xl md:text-7xl xl:text-9xl font-light tracking-wide">Contato</h1>
+        <div className="flex flex-col w-full">
+          <p className="max-w-2xl text-right font-light text-xl md:text-2xl xl:text-3xl">Whatsapp: {userProps.cel}</p>
+          <p className="max-w-2xl text-right font-light text-xl md:text-2xl xl:text-3xl">{userProps.email}</p>
         </div>
       </motion.header>
     </div >
