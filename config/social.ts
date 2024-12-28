031e8501f0638559a9097e678dad9c794b4a7fd2
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-28 18:09:54 -0300

    feat: add icon to socials.ts config

--- config/social.ts
+++ config/social.ts
@@ -1,24 +1,30 @@
-import userProps from "./user";
+import { Github, Instagram, Linkedin, LucideIcon, Mail } from "lucide-react";
+import { IconType } from "react-icons";
 
 export type social = {
+  icon: LucideIcon | IconType;
   name: string;
   link: string;
 }
 
 export const socialProps: social[] = [
   {
+    icon: Github,
     name: 'Github',
     link: 'https://github.com/Guilherme-RNovaes'
   },
   {
+    icon: Linkedin,
     name: 'Linkedin',
     link: 'https://www.linkedin.com/in/guilherme-r-novaes/'
   },
   {
+    icon: Instagram,
     name: 'Instagram',
     link: 'https://www.instagram.com/gr.novaes/'
   },
   {
+    icon: Mail,
     name: 'Email',
     link: 'mailto:guilherme00rogerio@gmail.com'
   },
