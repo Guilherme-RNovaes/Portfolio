1f111081724df0044c529af3d29ae660820e7d33
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-02 12:57:22 -0300

    refactor: change aboutContact default text

--- config/user.ts
+++ config/user.ts
@@ -26,7 +26,7 @@ const userProps: User =
   aboutSkills: 'Descubra as tecnologias que fazem parte do meu dia a dia! Aqui, você encontrará as ferramentas, linguagens, aplicativos e sistemas que utilizo para desenvolver com eficiência.',
   aboutServices: 'Transformo ideias em soluções digitais. Com expertise em React e desenvolvimento full-stack, ofereço serviços personalizados para atender às suas necessidades, incluindo:',
   aboutProjects: 'Conheça alguns dos projetos que desenvolvi! Aqui estão exemplos de soluções digitais que combinam criatividade, funcionalidade e tecnologia para atender às necessidades de cada cliente.',
-  aboutContact: 'Vamos conversar? Entre em contato e vamos transformar suas ideias em realidade. Estou à disposição para criar soluções digitais sob medida para você ou sua empresa!',
+  aboutContact: 'Entre em contato e vamos transformar suas ideias em realidade. Estou à disposição para criar soluções digitais sob medida para você ou sua empresa!',
   imageUrl: '/images/user-image.png',
   curriculum: 'https://drive.google.com/file/d/18hcNdNSHwiQO6KUnBdv40D2g5co_U9ai/view?usp=sharing',
 }
