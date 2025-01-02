7758286c830d4b1a35edd6e10295b1aaa16a4220
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-02 12:59:44 -0300

    refactor: change the entire hero section

--- app/(home)/_components/hero.tsx
+++ app/(home)/_components/hero.tsx
@@ -1,56 +1,105 @@
 'use client'
 
-import { socialProps } from "@/config/social"
-import userProps from "@/config/user"
-import { ArrowUpRight } from "lucide-react"
-import { motion } from "motion/react"
-import Link from "next/link"
+import { socialProps } from "@/config/social";
+import userProps from "@/config/user";
+import gsap from "gsap";
+import { ArrowUpRight } from "lucide-react";
+import { motion } from "motion/react";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 const HeroSection = () => {
-  return (
-    <div className="min-h-screen w-screen flex flex-col items-center justify-between bg-background py-14 pt-28 px-8">
-      <motion.div
-        initial={{ x: -100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.7, ease: "easeInOut" }}
-        className="flex flex-row w-full items-start justify-center"
-      >
-        <h1 className="text-6xl text-center md:text-6xl lg:text-8xl xl:text-[8rem] 2xl:text-[11rem] font-semibold uppercase">{userProps.name} {userProps.lastName}</h1>
-      </motion.div>
-      <motion.div
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.7, ease: "easeInOut" }}
-        className="flex flex-col lg:flex-row gap-9 w-full justify-between items-start"
-      >
-        <h2 className="font-light text-lg md:text-xl lg:text-xl tracking-[5px] md:tracking-[8px] text-left">{userProps.profession}</h2>
-        <p className="max-w-lg text-left font-light text-gray-500 text-lg md:text-xl lg:text-xl">{userProps.hero}</p>
-      </motion.div>
+  const textContainerRef = useRef(null);
 
-      <motion.div
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.7, ease: "easeInOut" }}
-        className="flex items-baseline justify-between w-full"
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      ".reveal-hero",
+      { y: "100%" },
+      {
+        y: "0%",
+        ease: "power4.out",
+        duration: 0.7,
+        stagger: 0.05,
+      }
+    );
+
+    tl.fromTo(
+      ".reveal-text",
+      { y: "100%", opacity: 0 },
+      {
+        y: "0%",
+        opacity: 1,
+        ease: "power4.out",
+        duration: 1,
+        stagger: 0.5,
+      }
+    );
+  }, []);
+
+  return (
+    <div className="min-h-screen w-screen flex flex-col items-center justify-between bg-background pb-16 pt-28 px-8">
+      <div
+        ref={textContainerRef}
+        className="flex flex-col w-full overflow-hidden items-center justify-center"
+      >
+        <h1 className="overflow-hidden text-center text-[9vw] md:text-[10vw] lg:text-[10vw] xl:text-[10rem] font-panchang font-regular uppercase">
+          {userProps.name.split("").map((char, index) => (
+            <span key={index} className="reveal-hero inline-block overflow-hidden translate-y-[115%]">
+              {char}
+            </span>
+          ))}{" "}
+        </h1>
+        <h1 className="overflow-hidden text-center text-[9vw] md:text-[10vw] lg:text-[10vw] xl:text-[10rem] font-panchang font-regular uppercase">
+          {userProps.lastName.split("").map((char, index) => (
+            <span key={index} className="reveal-hero inline-block overflow-hidden translate-y-[115%]">
+              {char}
+            </span>
+          ))}
+        </h1>
+      </div>
+
+      <div
+        ref={textContainerRef}
+        className="flex flex-col lg:flex-row gap-9 w-full justify-between items-start overflow-hidden"
+      >
+        <h2 className="overflow-hidden font-light text-lg md:text-xl lg:text-xl text-left">
+          <span className="reveal-text inline-block overflow-hidden translate-y-[115%] opacity-0 tracking-[5px] md:tracking-[8px]">
+            {userProps.profession}
+          </span>
+        </h2>
+        <p className="overflow-hidden max-w-lg text-left font-light text-gray-500 text-lg md:text-xl lg:text-xl">
+          <span className="reveal-text inline-block overflow-hidden translate-y-[115%] opacity-0">
+            {userProps.hero}
+          </span>
+        </p>
+      </div>
+
+      <div
+        ref={textContainerRef}
+        className="reveal-text flex items-baseline justify-between w-full opacity-0"
       >
         <div>
-          <h2 className="font-light text-lg md:text-xl lg:text-xl tracking-[5px] md:tracking-[8px] text-left">Socials:</h2>
+          <h2 className="font-light text-lg md:text-xl lg:text-xl tracking-[5px] md:tracking-[8px] text-left">
+            Socials:
+          </h2>
         </div>
         <div className="flex flex-col md:flex-row md:gap-10">
           {socialProps.map((props, index) => (
             <Link
               href={props.link}
               key={index}
-              className="-m-1 uppercase flex flex-row items-center gap-1 group"
+              className="uppercase flex flex-row items-center gap-1 group"
             >
               {props.name}
               <ArrowUpRight className="w-4 h-4 group-hover:translate-x-1 group-hover:-translate-y-1 transition-all" />
             </Link>
           ))}
         </div>
-      </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HeroSection;
