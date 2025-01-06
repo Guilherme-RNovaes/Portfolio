74a9cc93ce7f25d05fee1e906c38dd011e254617
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-06 07:25:02 -0300

    feat: add lenis smooth scrool support

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,10 +3,11 @@ import About from "./_components/about";
 import Services from "./_components/services";
 import Portfolio from "./_components/portfolio";
 import Contact from "./_components/contact";
+import { ReactLenis, useLenis } from "lenis/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-background">
+    <ReactLenis root className="min-h-screen flex flex-col items-center justify-center bg-background">
       <section id="home">
         <HeroSection />
       </section>
@@ -22,6 +23,6 @@ export default function Home() {
       <section id="contact">
         <Contact />
       </section>
-    </div>
+    </ReactLenis>
   );
 }
--- app/portfolio/page.tsx
+++
@@ -1,9 +0,0 @@
-const Portfolio = () => {
-  return (
-    <div className="min-h-screen bg-zinc-100">
-      Hello world
-    </div>
-  )
-}
-
-export default Portfolio;
