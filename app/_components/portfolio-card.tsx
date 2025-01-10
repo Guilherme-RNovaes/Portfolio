a34691dfd898250ed70eb1c1138cf7ea6e68040b
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-10 13:00:31 -0300

    refactor: refactor portfolio card component and remove dialog

--- app/_components/portfolio-card.tsx
+++ app/_components/portfolio-card.tsx
@@ -4,11 +4,7 @@ import { Button } from "@/app/_components/ui/button";
 import { Github, Link2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { motion } from "framer-motion";
 import { Badge } from "./ui/badge";
-import { Dialog, DialogTrigger, DialogContent } from "./ui/dialog";
-import { fromLeftAnimation, fromRightAnimation } from "@/config/animations";
 
 type Props = {
   direction: string;
@@ -23,21 +19,14 @@ type Props = {
 
 const PortfolioCard = ({ direction, name, img, description, stacks, services, link, source }: Props) => {
   const isRightDirection = direction === 'right'
+
   return (
-    <motion.div
-      variants={isRightDirection ? fromLeftAnimation({ duration: 0.8, delay: 0.5 }) : fromRightAnimation({ duration: 0.8, delay: 0.5 })}
-      initial="hidden"
-      whileInView="visible"
-      className={`flex flex-col ${isRightDirection ? 'lg:flex-row-reverse' : 'lg:flex-row'} w-full items-center justify-between mb-12 gap-x-6`}
+    <div
+      className={`flex flex-col ${isRightDirection ? 'lg:flex-row-reverse -translate-x-[10%]' : 'lg:flex-row translate-x-[10%]'} portfolio-card w-full items-center justify-between mb-12 gap-x-6`}
     >
-      <Dialog>
-        <DialogTrigger className={`relative w-[300px] h-[300px] md:w-[600px] md:h-[450px] mb-8 md:mb-0 rounded-xl cursor-zoom-in overflow-hidden group`}>
-          <Image src={img} fill alt="project image" className="rounded-xl object-cover transition-transform duration-300 ease-in-out group-hover:scale-105" />
-        </DialogTrigger>
-        <DialogContent className="md:min-w-[1080px] md:pr-12 md:py-2 md:pl-2">
-          <Image src={img} width={1920} height={1080} alt="project image" className="rounded-lg" />
-        </DialogContent>
-      </Dialog>
+      <div className={`relative w-[300px] h-[300px] md:w-[600px] md:h-[450px] mb-8 md:mb-0 rounded-xl cursor-zoom-in overflow-hidden group`}>
+        <Image src={img} fill alt="project image" className="rounded-xl object-cover transition-transform duration-300 ease-in-out group-hover:scale-105" />
+      </div>
       <div className={`flex flex-col items-center gap-6 ${isRightDirection ? 'md:items-start' : 'md:items-end md:text-right'}`}>
         <h1 className="text-4xl md:text-7xl uppercase">{name}</h1>
         <p className="text-xl md:text-2xl text-zinc-400 font-light max-w-xs md:max-w-xl truncate text-wrap">{description}</p>
@@ -71,7 +60,7 @@ const PortfolioCard = ({ direction, name, img, description, stacks, services, li
           </Button>
         </div>
       </div>
-    </motion.div >
+    </div >
   )
 }
 
