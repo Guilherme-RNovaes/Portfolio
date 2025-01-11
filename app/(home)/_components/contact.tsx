7bef4f482c22b0bc74d3cf8cd47ed084acafa3c6
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-11 18:50:43 -0300

    feat: finalize contact section front-end

--- app/(home)/_components/contact.tsx
+++ app/(home)/_components/contact.tsx
@@ -5,6 +5,11 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
+import { Button } from "@/app/_components/ui/button";
+import Link from "next/link";
+import { socialProps } from "@/config/social";
+import { ArrowUpRight, CornerDownRight } from "lucide-react";
+import { RiArrowRightUpLine } from "react-icons/ri";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,7 +18,6 @@ const Contact = () => {
   const containerRef = useRef(null);
 
   useGSAP(() => {
-    const containerElement = containerRef.current;
     gsap.fromTo(
       ".contact-text-reveal",
       { y: "115%", opacity: "100%" },
@@ -23,15 +27,15 @@ const Contact = () => {
         duration: 0.7,
         stagger: 0.5,
         scrollTrigger: {
-          trigger: containerElement,
+          trigger: containerRef.current,
           start: "top 80%",
           end: "top 50%",
           toggleActions: "play none none none"
         }
       })
-  }, [])
+  })
   return (
-    <div className="min-h-screen w-screen flex flex-col items-center light bg-background text-foreground pt-20 md:pt-40 px-8 md:px-24 xl:px-36">
+    <div className="min-h-screen w-screen flex flex-col items-center light bg-background text-foreground py-20 md:py-40 px-8 md:px-24 xl:px-36">
       <header
         className="flex flex-col md:flex-row items-start md:items-center justify-between gap-10 md:gap-4 xl:gap-0 w-full mb-20 md:mb-40"
       >
@@ -48,8 +52,28 @@ const Contact = () => {
         </div>
       </header>
       <div className="flex flex-col items-center justify-center gap-20 w-full dark bg-background text-foreground rounded-xl px-5 md:px-36 py-16">
-        <h1 className="text-4xl md:text-6xl lg:text-7xl uppercase">Vamos fazer acontecer</h1>
-        <h1 className="text-xl max-w-2xl text-center md:text-2xl">{userProps.aboutContact}</h1>
+        <header>
+          <h1 className="text-5xl text-center md:text-8xl lg:text-9xl font-semibold uppercase">Vamos fazer acontecer</h1>
+        </header>
+        <main className="flex flex-col items-center justify-between gap-10">
+          <p className="text-xl max-w-2xl text-center md:text-2xl text-zinc-400">{userProps.aboutContact}</p>
+          <button className="w-[250px] h-[80px] md:w-[320px] md:h-[100px] bg-white rounded-xl text-black uppercase font-semibold text-xl md:text-2xl group">
+            <Link
+              href="https://api.whatsapp.com/send?phone=5516988448896&text=Ol%C3%A1,%20Guilherme!" target="_blank" rel="noreferrer noopener"
+              className="flex items-center justify-center gap-2"
+            >
+              Entre em contato!
+              <ArrowUpRight className="w-5 h-5 md:w-15 md:h-15 group-hover:translate-x-1 group-hover:-translate-y-1 transition-all" />
+            </Link>
+          </button>
+        </main>
+        <footer className="flex flex-col items-center text-zinc-300">
+          <p className="uppercase text-xl font-semibold tracking-wide">para mais informações</p>
+          <div className="flex flex-row items-center gap-1">
+            <CornerDownRight className="w-3 h-3" />
+            <p className="font-medium">guilherme00rogerio@gmail.com</p>
+          </div>
+        </footer>
       </div>
     </div >
   )
