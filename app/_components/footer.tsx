97f8ddd1bf11ad2d951ed6e690b2937e45de06c4
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-15 20:22:07 -0300

    refactor: ad final footer design

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -7,10 +7,9 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col light bg-background text-foreground items-center justify-center px-8 py-16 md:p-16 lg:px-36 gap-8 border-t border-1 border-sub">
-      <div className="flex flex-row items-start justify-between pb-10 w-full">
-        <h1 className="font-semibold text-2xl">GN</h1>
-        <div className="flex flex-col">
+    <footer className="flex flex-col relative overflow-hidden light bg-background text-foreground items-center justify-center px-8 py-16 md:p-16 lg:px-36 gap-8 border-t border-1 border-sub">
+      <div className="flex flex-row md:flex-col items-start justify-between gap-20 pb-10 w-full">
+        <div className="flex flex-col md:flex-row gap-5 md:gap-10 w-full">
           {NavigationProps.map((props, index) => (
             <Link
               href={props.href}
@@ -22,7 +21,7 @@ const Footer = () => {
             </Link>
           ))}
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col md:flex-row gap-5 md:gap-10 justify-end w-full">
           {socialProps.map((props, index) => (
             <Link
               href={props.link}
@@ -36,10 +35,11 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <div className="text-sub flex flex-col w-full">
+      <div className="text-sub flex flex-col w-full pb-16 md:pb-20 lg:pb-36">
         <p className="text-2xl md:text-5xl font-semibold">©2025 Guilherme Novaes</p>
         <p className="text-2xl md:text-5xl font-semibold" >Todos os direitos reservados</p>
       </div>
+      <h1 className="font-panchang text-foreground uppercase text-[12vw] tracking-[5px] md:tracking-[10px] font-bold absolute -bottom-6 md:-bottom-14 lg:-bottom-16 xl:-bottom-20">guilherme</h1>
     </footer>
   )
 }
