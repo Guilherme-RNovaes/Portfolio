7f5c0483ac162bae384a3ba66d7c59e9c76007da
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-16 09:33:55 -0300

    refactor: clean navbar-items code

--- app/_components/navbar-items.tsx
+++ app/_components/navbar-items.tsx
@@ -3,39 +3,10 @@
 import { Button } from '@/app/_components/ui/button';
 import Link from 'next/link';
 import NavigationProps from '@/config/navigation';
-import { useEffect, useState } from 'react';
 
 const navigationItem = NavigationProps;
 
 const NavbarItems = () => {
-  {/*
-  const [activeSection, setActiveSection] = useState<string | null>(null);
-
-  useEffect(() => {
-    const observerOptions = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.1,
-    };
-
-    const observerCallback = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActiveSection(entry.target.id);
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(observerCallback, observerOptions);
-
-    const sections = document.querySelectorAll('section');
-    sections.forEach((section) => observer.observe(section));
-
-    return () => {
-      sections.forEach((section) => observer.unobserve(section));
-    };
-  }, []);
-*/}
   return (
     <>
       <div className="flex bg-black/30 backdrop-blur-xl shadow-xl shadow-black/20 rounded-xl flex-row gap-2 px-6">
