20f442b54cba262cc14872cc8a112a5086e943b8
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-23 22:13:17 -0300

    refactor: refactor skills component header and remove marquee component

--- app/(home)/_components/skills.tsx
+++ app/(home)/_components/skills.tsx
@@ -1,55 +1,85 @@
 'use client'
 
-import { motion } from "motion/react";
-import { fromBottomAnimation } from "@/config/animations";
-import Marquee from "./marquee";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
 import userProps from "@/config/user";
-import SkillCard from "./skill-card";
-import { ArrowRightCircle, LifeBuoy, MonitorSmartphone } from "lucide-react";
+import SkillCardsContainer from "./skill-card";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Skills = () => {
+
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+    const skillContainerElement = containerRef.current;
+    gsap.fromTo(
+      ".skilltext-reveal",
+      { y: "115%" },
+      {
+        y: "0%",
+        ease: "power4.inOut",
+        duration: 0.7,
+        stagger: 0.5,
+        scrollTrigger: {
+          trigger: skillContainerElement,
+          start: "top 80%",
+          end: "top 50%",
+          toggleActions: "play play reverse reverse"
+        }
+      })
+    gsap.fromTo(
+      ".skill-card",
+      { opacity: 0 },
+      {
+        x: "0%",
+        opacity: 1,
+        duration: 1,
+        ease: "power.inOut",
+        stagger: 1,
+        scrollTrigger: {
+          trigger: skillContainerElement,
+          start: "top 100%",
+          end: "bottom 100%",
+          scrub: true,
+          markers: false,
+        },
+      }
+    )
+    gsap.from(
+      ".aboutskill-reveal",
+      {
+        scrollTrigger: {
+          trigger: skillContainerElement,
+          start: "-10% 90%",
+          end: "-10% 20%",
+          scrub: true,
+          markers: false,
+        },
+        opacity: 0.2,
+        stagger: 0.01,
+      },
+    )
+  })
   return (
-    <div className="w-screen flex flex-col items-center pt-20 px-12 md:px-24 xl:px-36">
-      <motion.header
-        variants={fromBottomAnimation({ duration: 0.7, delay: 0 })}
-        initial="hidden"
-        whileInView="visible"
-        className="flex flex-col items-center justify-center w-full mb-12 gap-2"
+    <div className="w-screen flex flex-col items-center pt-20 px-8 md:px-24 xl:px-36">
+      <header
+        className="flex flex-col items-end justify-between gap-10 w-full mb-20 md:mb-40"
       >
-        <h1 className="text-3xl md:text-5xl font-semibold">Minhas Skills</h1>
-        <p className="max-w-3xl text-center font-light text-sub text-xl md:text-2xl mb-4">{userProps.aboutSkills}</p>
-      </motion.header>
-      <div className="flex flex-col lg:flex-row items-center justify-between gap-4 mb-20">
-        <motion.div
-          variants={fromBottomAnimation({ duration: 0.7, delay: 0.5 })}
-          initial="hidden"
-          whileInView="visible"
-        >
-          <SkillCard icon={ArrowRightCircle} title="Landing Pages que convertem" text="Precisa atrair clientes? Crio páginas de alta performance, otimizadas para SEO e com design voltado para conversões." />
-        </motion.div>
-        <motion.div
-          variants={fromBottomAnimation({ duration: 0.7, delay: 0.7 })}
-          initial="hidden"
-          whileInView="visible"
-        >
-          <SkillCard icon={MonitorSmartphone} title="Design responsivo" text="Sites incríveis, que funcionam perfeitamente em qualquer dispositivo. Proporciono uma experiência fluida tanto para desktop quanto para mobile." />
-        </motion.div>
-        <motion.div
-          variants={fromBottomAnimation({ duration: 0.7, delay: 0.9 })}
-          initial="hidden"
-          whileInView="visible"
-        >
-          <SkillCard icon={LifeBuoy} title="Suporte técnico e manutenção" text="Não basta ter um site incrível, ele precisa estar sempre atualizado. Ofereço suporte contínuo para garantir que sua plataforma esteja sempre no topo." />
-        </motion.div>
-      </div>
-      <motion.div
-        variants={fromBottomAnimation({ duration: 0.7, delay: 0.5 })}
-        initial="hidden"
-        whileInView="visible"
-        className="container mx-auto w-full flex justify-center items-center"
-      >
-        <Marquee />
-      </motion.div>
+        <div ref={containerRef} className="overflow-hidden w-full">
+          <h1 className="skilltext-reveal translate-y-[115%] text-4xl md:text-7xl xl:text-9xl font-light">Como posso ajudar</h1>
+        </div>
+        <div ref={containerRef} className="max-w-2xl text-right">
+          {userProps.aboutSkills.split("").map((char, i) => (
+            <span key={i} className="aboutskill-reveal font-light text-xl md:text-2xl xl:text-3xl">
+              {char}
+            </span>
+          ))}
+        </div>
+      </header>
+      <SkillCardsContainer />
     </div >
   )
 }
