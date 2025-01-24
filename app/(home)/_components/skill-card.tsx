1aa8daf664cfd31eb3df2010589b2dce8cbba3b3
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-23 22:11:14 -0300

    refactor: change the entire skill-card component

--- app/(home)/_components/skill-card.tsx
+++ app/(home)/_components/skill-card.tsx
@@ -1,18 +1,90 @@
-import { LucideIcon } from "lucide-react";
+"use client";
+
+import { ArrowRightCircle, LifeBuoy, LucideIcon, MonitorSmartphone } from "lucide-react";
 
 interface skillCardProps {
   icon: LucideIcon;
+  cardNum: string;
   title: string;
   text: string;
-}
-const SkillCard = ({ icon: Icon, title, text }: skillCardProps) => {
-  return (
-    <div className="flex flex-col max-w-[350px] h-[450px] justify-start px-8 py-8 xl:py-16 gap-6 border border-sub hover:border-foreground rounded-xl hover:-translate-y-5 transition-all">
-      <Icon className="w-8 md:w-16 h-8 md:h-16" />
-      <h1 className="text-2xl md:text-3xl xl:text-4xl font-light">{title}</h1>
-      <p className="text-lg md:text-xl font-light text-sub">{text}</p>
-    </div>
-  )
+  liOne: string;
+  liTwo: string;
+  liThree: string;
 }
 
-export default SkillCard;
+const SkillCard = ({ icon: Icon, cardNum, title, text, liOne, liTwo, liThree }: skillCardProps) => {
+  return (
+    <div className="skill-card bg-background flex flex-col w-full border-t border-sub items-center justify-start md:px-8 pt-2 pb-36 xl:pb-56">
+      <div className="flex flex-row w-full items-start py-2 justify-between">
+        <p className="text-xl w-[20%] md:w-[50%] md:text-4xl xl:text-6xl font-semibold">({cardNum})</p>
+        <div className="flex flex-col w-full gap-16">
+          <div className="flex flex-row w-full justify-between">
+            <h1 className="text-xl w-full md:text-4xl xl:text-6xl font-light">{title}</h1>
+            <Icon className="w-8 md:w-16 h-8 md:h-16 text-sub" />
+          </div>
+          <div className="flex flex-col gap-10">
+            <p className="text-xl md:text-2xl max-w-xl font-light text-sub">{text}</p>
+            <ul>
+              <li className="border-b border-sub py-2 flex flex-row gap-6 text-xl md:text-3xl">
+                <span className="w-12 text-lg text-sub">01</span>
+                <p>{liOne}</p>
+              </li>
+              <li className="border-b border-sub py-2 flex flex-row gap-6 text-xl md:text-3xl">
+                <span className="w-12 text-lg text-sub">02</span>
+                {liTwo}
+              </li>
+              <li className="border-b border-sub py-2 flex flex-row gap-6 text-xl md:text-3xl">
+                <span className="w-12 text-lg text-sub">03</span>
+                {liThree}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SkillCardsContainer = () => {
+  return (
+    <div
+      className="flex flex-col items-center w-full justify-between relative"
+    >
+      <div className="sticky top-32 md:top-56 w-full h-full">
+        <SkillCard
+          icon={ArrowRightCircle}
+          cardNum="01"
+          title="Landing Pages que convertem"
+          text="Precisa atrair clientes? Crio páginas de alta performance, otimizadas para SEO e com design voltado para conversões."
+          liOne="Conquiste Mais Clientes com Sites Otimizados"
+          liTwo="Páginas de Alta Performance para Seu Negócio"
+          liThree="Design Focado em Conversões e SEO"
+        />
+      </div>
+      <div className="sticky top-[200px] md:top-[330px] w-full h-full">
+        <SkillCard
+          icon={MonitorSmartphone}
+          cardNum="02"
+          title="Design responsivo"
+          text="Sites incríveis, que funcionam perfeitamente em qualquer dispositivo. Proporciono uma experiência fluida tanto para desktop quanto para mobile."
+          liOne="Experiência Perfeita em Qualquer Dispositivo"
+          liTwo="Sites Responsivos para Desktop e Mobile"
+          liThree="Design Funcional e Impressionante"
+        />
+      </div>
+      <div className="sticky top-[300px] md:top-[430px] w-full h-full">
+        <SkillCard
+          icon={LifeBuoy}
+          cardNum="03"
+          title="Suporte técnico e manutenção"
+          text="Não basta ter um site incrível, ele precisa estar sempre atualizado. Ofereço suporte contínuo para garantir que sua plataforma esteja sempre no topo."
+          liOne="Seu Site Sempre Atualizado e no Topo"
+          liTwo="Manutenção Contínua para Sua Plataforma"
+          liThree="Suporte Essencial para Crescer Online"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SkillCardsContainer;
