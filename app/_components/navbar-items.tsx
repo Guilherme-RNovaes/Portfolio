56a0611ba1f5d46b656f479a8e55f15527b46f77
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-01-31 08:49:34 -0300

    fix: fix navbar items to display hidden on sm screen

--- app/_components/navbar-items.tsx
+++ app/_components/navbar-items.tsx
@@ -8,7 +8,7 @@ const navigationItem = NavigationProps;
 const NavbarItems = () => {
   return (
     <>
-      <div className="flex bg-black/30 backdrop-blur-xl shadow-xl shadow-black/20 rounded-xl flex-row gap-10 px-10 py-2">
+      <div className="hidden md:flex bg-black/30 backdrop-blur-xl shadow-xl shadow-black/20 rounded-xl flex-row gap-10 px-10 py-2">
         {navigationItem.map((props, index) => (
           <AnimatedLink key={index} href={props.href} className='text-white font-medium text-sm'>
             {props.name}
